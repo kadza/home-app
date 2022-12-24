7dc437a2c0781aae947bf0d88c0b19ae9723bec5
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-12-24 13:23:13 +0000

    Reorganized rooms to match real house

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,49 @@
 <script>
-	import Floor_0 from "./floor-0.svelte";
-  import Floor_1 from "./floor-1.svelte";
+	import Room from "./room.svelte";
+  import {isGuestLightOn} from "../home-store";
+
+  const toggleGuestLight = () =>  {
+    isGuestLightOn.update(n => !n);
+  }
 </script>
 
 <style>
   .home {
   display: flex;
   }
+  .floor {
+    display: grid;
+    grid-template-columns: repeat(18, 2fr);
+    grid-template-rows: repeat(19, 2fr);
+    height: calc(100vh - 50px);
+    aspect-ratio: 1/1;
+    row-gap: 0.25rem;
+    column-gap: 0.25rem;
+  }
 </style>
 
 <div class="home">
-  <Floor_0 />
-  <Floor_1 />
+  <div class="floor">
+    <Room id="bay" rowStart={1} columnStart={11} rowSpan={1} columnSpan={6} />
+    <Room id="living" rowStart={2} columnStart={10} rowSpan={8} columnSpan={8} />
+    <Room id="guest" rowStart={2} columnStart={1} rowSpan={5} columnSpan={4} roomClicked={toggleGuestLight} isLightOn={$isGuestLightOn}/>
+    <Room id="bath-0" rowStart={2} columnStart={5} rowSpan={4} columnSpan={3} />
+    <Room id="stairs-0" rowStart={2} columnStart={8} rowSpan={5} columnSpan={2} />
+    <Room id="hall-0" rowStart={6} columnStart={5} rowSpan={1} columnSpan={3} />
+    <Room id="boiler" rowStart={7} columnStart={1} rowSpan={3} columnSpan={5} />
+    <Room id="store" rowStart={7} columnStart={6} rowSpan={3} columnSpan={4} />
+    <Room id="garage" rowStart={10} columnStart={1} rowSpan={9} columnSpan={9} />
+    <Room id="entrance" rowStart={10} columnStart={10} rowSpan={4} columnSpan={3} />
+    <Room id="kitchen" rowStart={10} columnStart={13} rowSpan={4} columnSpan={5} />
+  </div>
+  <div class="floor">
+    <Room id="bedroom" rowStart={2} columnStart={1} rowSpan={8} columnSpan={7} />
+    <Room id="stairs-1" rowStart={2} columnStart={8} rowSpan={6} columnSpan={4} />
+    <Room id="blanka" rowStart={2} columnStart={12} rowSpan={6} columnSpan={6} />
+    <Room id="hall-1" rowStart={8} columnStart={8} rowSpan={2} columnSpan={5} />
+    <Room id="blanka" rowStart={8} columnStart={13} rowSpan={6} columnSpan={5} />
+    <Room id="wardrobe" rowStart={10} columnStart={1} rowSpan={4} columnSpan={4} />
+    <Room id="laundry" rowStart={10} columnStart={5} rowSpan={4} columnSpan={4} />
+    <Room id="bath-1" rowStart={10} columnStart={9} rowSpan={4} columnSpan={4} />
+  </div>
 </div>
--- src/routes/room.svelte
+++ src/routes/room.svelte
@@ -1,20 +1,22 @@
 <script>
   export let isLightOn = false;
-  $: color = isLightOn ? "yellow" : "grey";
+  $: color = isLightOn ? "#ffcc00" : "#d6d6c2";
   export let roomClicked = () => {};
   export let columnStart = 0;
   export let rowStart = 0;
   export let columnSpan = 0;
   export let rowSpan = 0;
+  export let id = "";
 </script>
 
 <style>
   .room {
     border: 1px;
-    border-color: black;
+    border-color: #d6d6c2;
     border-style: solid;
     border-radius: 3px;
   }
 </style>
 
-<div class="room" style="background-color: {color}; grid-area: {rowStart} / {columnStart} / span {rowSpan} / span {columnSpan}" on:click={roomClicked}></div>
+<div {id} class="room" style="background-color: {color}; grid-area: {rowStart} / {columnStart} / span {rowSpan} / span {columnSpan}" on:click={roomClicked}>
+</div>
