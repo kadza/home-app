46e46f47156639ec25dae5371fa0b550ce5526d7
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-12-25 21:49:05 +0000

    Refactored styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,10 @@
 </script>
 
 <style>
+   :global(body){
+        background-image: url("./bg.jpeg");
+        font-family: Nunito,sans-serif
+    }
   .home {
   display: flex;
   }
@@ -24,26 +28,26 @@
 
 <div class="home">
   <div class="floor">
-    <Room id="bay" rowStart={1} columnStart={11} rowSpan={1} columnSpan={6} />
-    <Room id="living" rowStart={2} columnStart={10} rowSpan={8} columnSpan={8} />
-    <Room id="guest" rowStart={2} columnStart={1} rowSpan={5} columnSpan={4} roomClicked={toggleGuestLight} isLightOn={$isGuestLightOn}/>
-    <Room id="bath-0" rowStart={2} columnStart={5} rowSpan={4} columnSpan={3} />
-    <Room id="stairs-0" rowStart={2} columnStart={8} rowSpan={5} columnSpan={2} />
-    <Room id="hall-0" rowStart={6} columnStart={5} rowSpan={1} columnSpan={3} />
-    <Room id="boiler" rowStart={7} columnStart={1} rowSpan={3} columnSpan={5} />
-    <Room id="store" rowStart={7} columnStart={6} rowSpan={3} columnSpan={4} />
-    <Room id="garage" rowStart={10} columnStart={1} rowSpan={9} columnSpan={9} />
-    <Room id="entrance" rowStart={10} columnStart={10} rowSpan={4} columnSpan={3} />
-    <Room id="kitchen" rowStart={10} columnStart={13} rowSpan={4} columnSpan={5} />
+    <Room id="bay" name="Bay" rowStart={1} columnStart={11} rowSpan={1} columnSpan={6} />
+    <Room id="living" name="Living" rowStart={2} columnStart={10} rowSpan={8} columnSpan={8} />
+    <Room id="guest" name="Guest" rowStart={2} columnStart={1} rowSpan={5} columnSpan={4} roomClicked={toggleGuestLight} isLightOn={$isGuestLightOn}/>
+    <Room id="bath-0" name="Bath" rowStart={2} columnStart={5} rowSpan={4} columnSpan={3} />
+    <Room id="stairs-0" name ="Stairs" rowStart={2} columnStart={8} rowSpan={5} columnSpan={2} />
+    <Room id="hall-0" name="Hall" rowStart={6} columnStart={5} rowSpan={1} columnSpan={3} />
+    <Room id="boiler" name="Boiler" rowStart={7} columnStart={1} rowSpan={3} columnSpan={5} />
+    <Room id="store" name="Store" rowStart={7} columnStart={6} rowSpan={3} columnSpan={4} />
+    <Room id="garage" name="Garage" rowStart={10} columnStart={1} rowSpan={9} columnSpan={9} />
+    <Room id="entrance" name="Entrance" rowStart={10} columnStart={10} rowSpan={4} columnSpan={3} />
+    <Room id="kitchen" name="Kitchen" rowStart={10} columnStart={13} rowSpan={4} columnSpan={5} />
   </div>
   <div class="floor">
-    <Room id="bedroom" rowStart={2} columnStart={1} rowSpan={8} columnSpan={7} />
-    <Room id="stairs-1" rowStart={2} columnStart={8} rowSpan={6} columnSpan={4} />
-    <Room id="blanka" rowStart={2} columnStart={12} rowSpan={6} columnSpan={6} />
-    <Room id="hall-1" rowStart={8} columnStart={8} rowSpan={2} columnSpan={5} />
-    <Room id="blanka" rowStart={8} columnStart={13} rowSpan={6} columnSpan={5} />
-    <Room id="wardrobe" rowStart={10} columnStart={1} rowSpan={4} columnSpan={4} />
-    <Room id="laundry" rowStart={10} columnStart={5} rowSpan={4} columnSpan={4} />
-    <Room id="bath-1" rowStart={10} columnStart={9} rowSpan={4} columnSpan={4} />
+    <Room id="bedroom" name="Bedroom" rowStart={2} columnStart={1} rowSpan={8} columnSpan={7} />
+    <Room id="stairs-1" name="Stairs" rowStart={2} columnStart={8} rowSpan={6} columnSpan={4} />
+    <Room id="blanka" name="Blanka" rowStart={2} columnStart={12} rowSpan={6} columnSpan={6} />
+    <Room id="hall-1" name="Hall" rowStart={8} columnStart={8} rowSpan={2} columnSpan={5} />
+    <Room id="leon" name="Leon" rowStart={8} columnStart={13} rowSpan={6} columnSpan={5} />
+    <Room id="wardrobe" name="Wardrobe" rowStart={10} columnStart={1} rowSpan={4} columnSpan={4} />
+    <Room id="laundry" name="Laundry" rowStart={10} columnStart={5} rowSpan={4} columnSpan={4} />
+    <Room id="bath-1" name="Bath" rowStart={10} columnStart={9} rowSpan={4} columnSpan={4} />
   </div>
 </div>
--- src/routes/room.svelte
+++ src/routes/room.svelte
@@ -1,22 +1,73 @@
 <script>
+  import LightBulb from "svelte-material-icons/Lightbulb.svelte";
+  import LightBulbOff from "svelte-material-icons/LightbulbOff.svelte";
+  import BlindsOpen from "svelte-material-icons/BlindsOpen.svelte";
+  import BlindsClosed from "svelte-material-icons/Blinds.svelte";
+  import Radiator from "svelte-material-icons/Radiator.svelte";
+  import RadiatorDisabled from "svelte-material-icons/RadiatorDisabled.svelte";
+  import RadiatorOff from "svelte-material-icons/RadiatorOff.svelte";
   export let isLightOn = false;
-  $: color = isLightOn ? "#ffcc00" : "#d6d6c2";
   export let roomClicked = () => {};
   export let columnStart = 0;
   export let rowStart = 0;
   export let columnSpan = 0;
   export let rowSpan = 0;
   export let id = "";
+  export let name = "";
 </script>
 
 <style>
   .room {
     border: 1px;
-    border-color: #d6d6c2;
-    border-style: solid;
-    border-radius: 3px;
+    border-radius: 2px;
+    box-shadow: 0 1px 5px rgba(0,0,0,.1);
+    background-color: rgba(0,0,0,.3);
+  }
+  .room small {
+    font-size: 0.75rem;
+    display: block;
+    color: rgba(255,255,255,.5);
+  }
+  .room button{
+    margin-top: 0.5rem;
+    background-color: transparent;
+    color: rgba(255,255,255,.85);
+    color: #fff;
+    display: inline-block;
+    font-weight: 400;
+    text-align: center;
+    vertical-align: middle;
+    background-color: transparent;
+    border: none;
+    font-size: 1rem;
+    border-radius: 2px;
+    cursor: pointer;
   }
 </style>
 
-<div {id} class="room" style="background-color: {color}; grid-area: {rowStart} / {columnStart} / span {rowSpan} / span {columnSpan}" on:click={roomClicked}>
+<div {id} class="room" style="grid-area: {rowStart} / {columnStart} / span {rowSpan} / span {columnSpan}">
+  <div style="padding: 0.5rem">
+  <small>{name}</small>
+  <button on:click={roomClicked}>
+    {#if isLightOn}
+      <LightBulb color={'white'} />
+    {:else}
+      <LightBulbOff color={'grey'} />
+    {/if}
+  </button>
+  <button on:click={roomClicked}>
+    {#if isLightOn}
+      <BlindsClosed color={'white'} />
+    {:else}
+      <BlindsOpen color={'grey'} />
+    {/if}
+  </button>
+  <button on:click={roomClicked}>
+    {#if isLightOn}
+      <Radiator color={'white'} />
+    {:else}
+      <RadiatorOff color={'grey'} />
+    {/if}
+  </button>
+</div>
 </div>
