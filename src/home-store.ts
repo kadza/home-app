2e81e701e2ded2b922baf68553b454dcc3449809
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2022-12-26 11:02:48 +0000

    Added temperature

--- src/home-store.ts
+++ src/home-store.ts
@@ -1,18 +1,26 @@
-import { PUBLIC_GUEST_LIGHT_FROM, PUBLIC_GUEST_LIGHT_TO, PUBLIC_MQTT_PASSWORD, PUBLIC_MQTT_URL, PUBLIC_MQTT_USER } from "$env/static/public";
+import { PUBLIC_EXTERNAL_TEMP, PUBLIC_GUEST_LIGHT_FROM, PUBLIC_GUEST_LIGHT_TO, PUBLIC_MQTT_PASSWORD, PUBLIC_MQTT_URL, PUBLIC_MQTT_USER } from "$env/static/public";
 import { connect } from "mqtt";
 import { writable } from "svelte/store";
 
 export const isGuestLightOn = writable(false);
+export const temperature = writable(0);
 
 let client = connect(PUBLIC_MQTT_URL, {username: PUBLIC_MQTT_USER, password: PUBLIC_MQTT_PASSWORD});
 
+client.on('connect', function () {
+  client.subscribe(PUBLIC_GUEST_LIGHT_FROM);
+  client.subscribe(PUBLIC_EXTERNAL_TEMP);
+})
+
 client.on('message', function (topic, message) {
-  console.log('topic is ' + topic)
-  console.log('message is ' + message)
+  console.log(`${topic} ${message.toString()}`)
 
   switch (topic) {
     case PUBLIC_GUEST_LIGHT_FROM:
       isGuestLightOn.set(message.toString() === "1");
+      break;
+    case PUBLIC_EXTERNAL_TEMP:
+      temperature.set(parseFloat(message.toString()));
       break;
   }
 })
---
+++ src/routes/temp.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { temperature } from "../home-store";
+import Thermometer from "svelte-material-icons/Thermometer.svelte";
+  </script>
+
+<style>
+  .temp{
+  color: #fff;
+  font-weight: 400;
+  display: flex;
+  align-items: center;
+}
+  </style>
+
+<div class="temp">
+  <Thermometer color={'white'} />
+<span>{$temperature} °C</span>
+
+</div>
