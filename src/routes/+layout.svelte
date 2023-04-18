29e5379d492cf54704acacef5c4940508bd9d0d2
Lukasz Kujawiak <lukasz.kujawiak@gmail.com>
2023-04-18 21:22:18 +0000

    Adjusted sizing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 </style>
 
 <div class="flex flex-row-reverse gap-2 bg-gray-800/40 px-2 py-2 shadow mb-4">
-  <div class="bg-white/10 px-2 py-1">
+  <div class="bg-white/10 px-1 py-1">
     <Clock />
   </div>
   <div class="bg-white/10 px-1 py-1">
